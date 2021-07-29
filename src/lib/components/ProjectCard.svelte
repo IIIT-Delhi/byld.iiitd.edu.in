de8929feb225c05823c1e1a292e8a1860b6fab76
Ananya Lohani <61914211+ananyalohani@users.noreply.github.com>
2021-07-29 23:32:54 +0530

    fix: Change card's default state

--- src/lib/components/ProjectCard.svelte
+++ src/lib/components/ProjectCard.svelte
@@ -7,14 +7,18 @@
 </script>
 
 <div
-  class="flex flex-col flex-1 p-4 text-gray-100 border-2 cursor-pointer space-y-2 transition-all duration-200 divide-y rounded-md hover:-translate-y-2 hover:shadow-xl hover:brightness-125 divide-sec-700 bg-sec-500 border-sec-500"
+  class="flex flex-col flex-1 p-4 space-y-2 text-gray-100 transition-all duration-200 border-2 divide-y rounded-md cursor-pointer group hover:-translate-y-1 hover:shadow-xl divide-sec-500 hover:divide-sec-700 hover:bg-sec-500 bg-sec-800 border-sec-500"
 >
   <div class="flex flex-row justify-between">
     <div class="flex flex-col items-start space-y-1">
       <h3 class="text-lg font-bold">{title}</h3>
       <p class="text-sm font-light text-gray-300">{subtitle}</p>
     </div>
-    <div><p class="italic font-semibold text-tert-700">{status}</p></div>
+    <div>
+      <p class="italic font-semibold text-tert-900 group-hover:text-tert-700">
+        {status}
+      </p>
+    </div>
   </div>
   <div class="flex flex-col justify-between flex-1 pt-2">
     <div>
@@ -26,7 +30,7 @@
     <div class="flex flex-row mt-3 space-x-2">
       {#each techStack as tech}
         <div
-          class="text-xs font-semibold py-1.5 px-2 rounded border border-sec-600 bg-sec-200 text-tert-200"
+          class="text-xs font-semibold py-1.5 px-2 rounded border bg-sec-400 border-sec-600 group-hover:border-sec-600 group-hover:bg-sec-200 text-tert-200"
         >
           {tech}
         </div>
